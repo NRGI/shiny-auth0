3baecc1cfa0b7794f3c73c89e44cb693c9b90ad0
Adina Petrean <adina@vitaminsoftware.com>
2020-05-14 14:27:28 +0300

    #1 Handle web sockets.

--- routes/apps.js
+++ routes/apps.js
@@ -5,11 +5,21 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn()
 var router = express.Router();
 
 var proxy = httpProxy.createProxyServer({
+  ws:true,
   target: {
       host: process.env.SHINY_HOST,
       port: process.env.SHINY_PORT
     }
 });
+
+var server = require('http').createServer(function(req, res) {
+  proxy.web(req, res, {
+    target: options[req.headers.host]
+  },function(e){
+    log_error(e,req);
+  });
+})
+
 
 proxy.on('error', function(e) {
   console.log('Error connecting');
@@ -29,6 +39,19 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
   setIfExists(proxyReq, 'x-auth0-name', req.user._json.name);
   setIfExists(proxyReq, 'x-auth0-picture', req.user._json.picture);
   setIfExists(proxyReq, 'x-auth0-locale', req.user._json.locale);
+});
+
+//
+// Listen to the `upgrade` event and proxy the
+// WebSocket requests as well.
+//
+server.on('upgrade', function (req, socket, head) {
+  console.log("proxying upgrade request", req.url);
+  proxy.ws(req, socket, head);
+});
+
+proxy.on('error', function (req, socket, head) {
+  console.log("error!!!")
 });
 
 /* Proxy all requests */
